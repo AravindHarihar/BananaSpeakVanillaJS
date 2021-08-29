d553dd78ea51c6a20978183e4443d8ff8f7fe63b
AravindHarihar <89657178+AravindHarihar@users.noreply.github.com>
2021-08-29 23:09:37 +0530

    Upto perfectly working logic
    Logic is working fine, the styling part is remaining

--- app.js
+++ app.js
@@ -2,11 +2,31 @@
 var btnTranslate = document.querySelector("#btn");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/mandalorian.json";
 
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+    console.log("Error Occurred" + error);
+    alert("Uh oh! try again later")
+}
 
 function clickHandler(){
-    outputDiv.innerText = "asdadjkajdhanbd  " + txtInput.value;
-    // console.log(outputDiv.innerText)
+    //outputDiv.innerText = "asdadjkajdhanbd  " + txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText)).then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        //console.log(json.contents.translated)
+    })
+    .catch(errorHandler)
 
 }
 
